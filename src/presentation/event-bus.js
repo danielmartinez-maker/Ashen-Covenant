ef255ea0babad15c0ea1d33010b8340fc0ced9f6
danielmartinez-maker <daniel.martinez@lomasaltas.edu.mx>
2026-08-21 15:10:27 -0600

    feat: register v7 presentation events

--- src/presentation/event-bus.js
+++ src/presentation/event-bus.js
@@ -3,7 +3,8 @@ const EVENT_TYPES = [
   'animation:action-end', 'animation:footstep', 'animation:reaction', 'animation:death', 'animation:resurrection',
   'combat:attack-start', 'combat:attack-impact', 'combat:enemy-telegraph', 'combat:enemy-impact', 'combat:boss-stagger',
   'impact:request', 'camera:profile', 'camera:impulse', 'music:state', 'music:stinger', 'music:duck', 'boss:signature-cue',
-  'cinematic:start', 'cinematic:end', 'cinematic:skip', 'loot:spawn', 'world:transition', 'presentation:error'
+  'cinematic:start', 'cinematic:end', 'cinematic:skip', 'loot:spawn', 'world:transition',
+  'presentation:combat-context', 'animation:clip-resolved', 'presentation:error'
 ];
 
 export const PRESENTATION_EVENT_TYPES = Object.freeze(Object.fromEntries(EVENT_TYPES.map((type) => [type.toUpperCase().replaceAll(':', '_').replaceAll('-', '_'), type])));
