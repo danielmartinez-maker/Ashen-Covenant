c136e1d50f8b49f7fca98c4feeb75d08d2d81651
danielmartinez-maker <daniel.martinez@lomasaltas.edu.mx>
2026-08-22 21:11:06 -0600

    fix: subscribe semantic loot pickup audio

--- src/presentation/system.js
+++ src/presentation/system.js
@@ -152,7 +152,7 @@ export class GamePresentationSystem {
     });
     [
       'context:changed', 'animation:footstep', 'combat:attack-start', 'combat:attack-impact', 'combat:enemy-telegraph', 'combat:enemy-impact',
-      'combat:boss-stagger', 'boss:signature-cue', 'loot:spawn', 'legacy:boss-defeated', 'legacy:sound'
+      'combat:boss-stagger', 'boss:signature-cue', 'loot:spawn', 'legacy:loot', 'legacy:boss-defeated', 'legacy:sound'
     ].forEach((type) => this.eventBus.on(type, (event) => this._resolveAudioEvent(event)));
     this.eventBus.on('presentation:error', (event) => { this.errorLog.push({ time: this.game?.clock ?? 0, ...event.detail }); this.errorLog.length = Math.min(40, this.errorLog.length); });
   }
